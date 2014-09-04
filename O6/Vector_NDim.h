3b419c9a9c39bdf8ebd47e208fecec95f60030b8
lhayward <l3haywar@uwaterloo.ca>
2014-09-04 17:08:07 -0400

    Added Vector_NDim class

---
+++ O6/Vector_NDim.h
@@ -0,0 +1,49 @@
+/**********************************************************************************************
+************************************ O(6) MODEL MONTE CODE ************************************
+***********************************************************************************************
+* Lauren E. Hayward Sierens
+***********************************************************************************************
+* File:   Vector_NDim.h
+**********************************************************************************************/
+
+#ifndef VECTOR_NDIM_H
+#define VECTOR_NDIM_H
+
+#include "MersenneTwister.h"
+
+using namespace std;
+
+class Vector_NDim 
+{
+  private:
+  public:
+    typedef unsigned int  uint;
+    
+    uint    N_;  //dimensionality of the vector
+    double* v_;  //components of the ND vector
+  
+    Vector_NDim(uint N, Vector_NDim* oldVec); //copy constructor
+    Vector_NDim(uint N, double val);
+    Vector_NDim(uint N, int val);
+    Vector_NDim(uint N, MTRand* randomGen);
+    virtual ~Vector_NDim();
+  
+    void         add(Vector_NDim* vec2);
+    void         clear();
+    double       dot(Vector_NDim* vec2);
+    double       dotForRange(Vector_NDim* vec2, uint start, uint end);
+    Vector_NDim* getAbsComponents();
+    Vector_NDim* getMultiple(double c);
+    Vector_NDim* getReflectionAndNormalize(Vector_NDim* r);
+    double       getSquare();
+    double       getSquareForRange(uint start, uint end);
+    Vector_NDim* getSqComponents();
+    void         multiply(double c);
+    void         normalize();
+    void         print();
+    void         reflectAndNormalize(Vector_NDim* r);
+    void         subtract(Vector_NDim* vec2);
+};
+
+#endif  /* VECTOR_NDIM_H */
+
